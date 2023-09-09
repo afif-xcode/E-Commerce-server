6bfe496a839c68be90a424e62627394e16c7218d
Tohidujjaman Hoque <tohi7479@gmail.com>
2023-09-09 15:37:44 +0530

    Creating orders controller

--- controllers/Orders.js
+++ controllers/Orders.js
@@ -1,6 +1,7 @@
 const Orders = require('../models/Orders');
 const User = require('../models/User');
 const Product = require('../models/Product');
+const mongoose = require('mongoose')
 const { StatusCodes } = require("http-status-codes");
 
 // create a order
@@ -22,10 +23,21 @@ exports.createOrder = async(req, res) => {
             message: "All fields are required",
             });
         }
+
+        const productIds = products.map(product => product.product);
+        const productsFound = await Product.find({ _id: { $in: productIds } });
+
+        if (productIds.length !== productsFound.length) {
+        return res.status(400).json({ error: 'Invalid product IDs' });
+        }
+
         // creating a new Order 
         const newOrder = await Orders.create(
             {   user : userId,
-                products,
+                products: products.map(productIndex => ({
+                    product: productIndex.product,
+                    quantity: productIndex.quantity,
+                })),
                 totalAmount,
                 paymentMod,
                 shippingAddress: shippingAdress,
@@ -66,12 +78,11 @@ exports.createOrder = async(req, res) => {
 }
 
 // show order by id
-exports.orderDetails = async (req,res) => {
+exports.getOrderDetails = async (req,res) => {
     try {
         // get order id from req.params 
-        const {orderId} = req.params;
-        
-        const orderDetails = await Orders.findById(orderId).populate('products').exec();
+        const orderId = req.body.orderId;
+        const orderDetails = await Orders.findById(orderId).populate([{path : 'user'}, {path : 'products', populate : 'product'}, {path : "shippingAddress"}]).exec();
         if(!orderDetails) {
             return res.status(StatusCodes.NOT_FOUND).json(
                 {
@@ -85,7 +96,8 @@ exports.orderDetails = async (req,res) => {
         return res.status(StatusCodes.OK).json(
             {
                 success : true,
-                message : "Order details fetch successfully"
+                message : "Order details fetch successfully",
+                data : orderDetails,
             }
         )
     }catch(error) {
@@ -105,23 +117,116 @@ exports.showAllOrdersofUser = async(req,res) => {
     try {
         const userId = req.user.id;
 
-        const user = await User.find(userId).populate('orders').exec();
-        const userOrder = user.orders;
+        const userActiveOrders = await Orders.find({
+            user: userId,
+            status: { $nin: ['Cancelled', 'Completed'] }
+        }).populate([{path : 'user'}, {path : 'products', populate : 'product'}, {path : "shippingAddress"}]).exec();
 
-        if(userOrder.length === 0) {
-            return res.status(StatusCodes.NOT_FOUND).json(
-                {
-                    success : false,
-                    message : "NO orders found",
-                }
-            )
-        }
+        const userCompletedOrders = await Orders.find({
+        user: userId,
+        status: "Completed"
+        }).populate([{path : 'user'}, {path : 'products', populate : 'product'}, {path : "shippingAddress"}]).exec();
+
+        const userCancelledOrders = await Orders.find({
+            user: userId,
+            status: "Cancelled"
+        }).populate([{path : 'user'}, {path : 'products', populate : 'product'}, {path : "shippingAddress"}]).exec();
 
         return res.status(StatusCodes.OK).json(
             {
                 success : true,
                 message : "Order fetched successfully",
-                data : userOrder,
+                data : {
+                    userActiveOrders,
+                    userCompletedOrders,
+                    userCancelledOrders
+                },
+            }
+        )
+    }catch(error) {
+        console.log("ERROR FROM ORDER CONTROLLER SHOW ALL ORDERS");
+        console.log(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+            {
+                success : false,
+                message : "Something went wrong while fetching show all orders"
+            }
+        )
+    }
+}
+
+// Change the status of an order by ID
+exports.updateOrderStatus = async (req, res) => {
+    try {
+    const { status, orderId } = req.body;
+      // Validate the provided status
+      const allowedStatusValues = ['Ordered', 'Confirmed', 'Shipped', 'Cancelled', 'Completed'];
+      if (!allowedStatusValues.includes(status)) {
+        return res.status(StatusCodes.BAD_REQUEST).json(
+            {
+                success : false,
+                message : "Invalid status value"
+            }
+        );
+      }
+  
+      // Update the order status using findOneAndUpdate
+      const updatedOrder = await Orders.findOneAndUpdate(
+        { _id: orderId },
+        { $set: { status } },
+        { new: true, runValidators: true }
+      );
+  
+      if (!updatedOrder) {
+        return res.status(StatusCodes.NOT_FOUND).json(
+            {
+                success : false,
+                message : "Order not found"
+            }
+        );
+      }
+      res.status(StatusCodes.OK).json(
+        {
+            success : true,
+            message : "Order status change successfully"
+        }
+      );
+    }catch(error) {
+        console.log("ERROR FROM ORDER CONTROLLER CHANGING ORDER STATUS");
+        console.log(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+            {
+                success : false,
+                message : "Something went wrong while changing order status"
+            }
+        )
+    }
+};
+
+// Get all orders for admin
+exports.showAllOrders = async(req,res) => {
+    try {
+        const userActiveOrders = await Orders.find({
+            status: { $nin: ['Cancelled', 'Completed'] }
+        }).populate([{path : 'user'}, {path : 'products', populate : 'product'}, {path : "shippingAddress"}]).exec();
+
+        const userCompletedOrders = await Orders.find({
+        status: "Completed"
+        }).populate([{path : 'user'}, {path : 'products', populate : 'product'}, {path : "shippingAddress"}]).exec();
+
+        const userCancelledOrders = await Orders.find({
+            status: "Cancelled"
+        }).populate([{path : 'user'}, {path : 'products', populate : 'product'}, {path : "shippingAddress"}]).exec();
+
+        return res.status(StatusCodes.OK).json(
+            {
+                success : true,
+                message : "Order fetched successfully",
+                data : {
+                    userActiveOrders,
+                    userCompletedOrders,
+                    userCancelledOrders
+                },
             }
         )
     }catch(error) {
--- routes/Orders.js
+++ routes/Orders.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    createOrder
+    createOrder,
+    updateOrderStatus,
+    getOrderDetails,
+    showAllOrdersofUser,
+    showAllOrders
 } = require("../controllers/Orders");
-const { auth, isCustomer } = require("../middlewares/auth");
+const { auth, isCustomer, isAdmin } = require("../middlewares/auth");
 
 router.post('/creatOrder', auth, isCustomer, createOrder);
+router.post('/updateOrderStatus', auth, isAdmin, updateOrderStatus);
+router.get('/getOrder', auth, isCustomer, getOrderDetails);
+router.get('/getAllOrders', auth, isCustomer, showAllOrdersofUser);
+router.get('/admin/Orders', auth, isAdmin, showAllOrders);
 
 module.exports = router;
